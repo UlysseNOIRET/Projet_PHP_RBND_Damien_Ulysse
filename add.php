a29381da68bd6e217e517ec0a4646b537b43dfb3
UlysseNOIRET <ulyssenoiret38@gmail.com>
2025-11-21 12:58:22 +0100

    Update add.php

--- add.php
+++ add.php
@@ -16,3 +16,22 @@ $note = $_POST['review_scores_value'] !== "" ? floatval($_POST['review_scores_va
  
 $max = $dbh->query("SELECT MAX(id) AS m FROM listings")->fetch()['m'];
 $newId = $max + 1;
+
+$stmt = $dbh->prepare("INSERT INTO listings (id, name, picture_url, host_name, host_thumbnail_url, price, neighbourhood_group_cleansed, review_scores_value)
+VALUES (:id,:n,:p,:h,:a,:pr,:c,:v)");
+
+$stmt->execute([
+    ':id' => $newId,
+    ':n' => $name,
+    ':p' => $picture,
+    ':h' => $host,
+    ':a' => $avatar,
+    ':pr' => $price,
+    ':c' => $city,
+    ':v' => $note
+]);
+
+$_SESSION['flash'] = "Annonce ajoutée.";
+header("Location: index.php");
+exit;
+?>
