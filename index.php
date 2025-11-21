fdfdf22f004badb59ed2e25bcb4e2cec366d2446
Dammssss <damien.bouziani@gmail.com>
2025-11-21 11:29:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-yftdugydryugcigveyihugbyifhurgyhuvgfbehugfviuhgfvhugyvifhgfuhgyufihgvfbhigudvih
+<?php
+require_once 'config.php';
+ 
+$perPage = 10;
+$page = isset($_GET['page']) ? max(1, intval($_GET['page'])) : 1;
+ 
+$validSorts = [
+    'name' => 'name',
+    'city' => 'neighbourhood_group_cleansed',
+    'price' => 'price',
+    'host' => 'host_name'
+];
+?>
